31d7ffebcc0930cca78e066f1c5643bbaabf1fba
sato <sato_m@mediaxis.jp>
2020-04-09 13:52:24 +0900

    Complete Navigation Between Pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import { NextPage } from "next";
+import Link from "next/link";
 import Hello from "@components/Hello";
 
 const Index: NextPage = () => {
   return (
     <div>
+      <Link href="/about">
+        <a title="About Page">About Page</a>
+      </Link>
       <Hello />
     </div>
   );
