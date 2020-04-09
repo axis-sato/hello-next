8d7a7bc48470be748e1f0d3c2a58ee240adc9632
sato <sato_m@mediaxis.jp>
2020-04-09 11:54:37 +0900

    ESLintとPrettierを導入

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import { NextPage } from 'next';
+import { NextPage } from "next";
 
 const Index: NextPage = () => {
   return (
-      <div>
-          <p>Hello Next.js</p>
-      </div>
-  )
+    <div>
+      <p>Hello Next.js</p>
+    </div>
+  );
 };
 
 export default Index;
